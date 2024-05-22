e2766225035915ba38f78808157d4864f4d5d707
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-05-22 19:27:17 +0200

    Create models.py
    models i use frequently

---
+++ models.py
@@ -0,0 +1,292 @@
+#torch
+import torch
+from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader, random_split
+from torch.cuda.amp import GradScaler
+#from torchvision.transforms import v2
+import torchvision.transforms as transforms
+import torchvision
+import torch.nn as nn
+from torch.optim import Adam
+import torch.nn.functional as F
+
+
+##############################################
+################ UNET GELU ###################
+##############################################
+
+class encoding_block_gelu(nn.Module):
+    def __init__(self,in_channels, out_channels):
+        super(encoding_block_gelu,self).__init__()
+        model = []
+        model.append(nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False))
+        model.append(nn.BatchNorm2d(out_channels))
+        model.append(nn.GELU())
+        model.append(nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False))
+        model.append(nn.BatchNorm2d(out_channels))
+        model.append(nn.GELU())
+        model.append(nn.Dropout(p=0.15))
+        self.conv = nn.Sequential(*model)
+    def forward(self, x):
+        return self.conv(x)  
+
+class unet_model_gelu(nn.Module):
+    def __init__(self,out_channels,features=[64, 128, 256, 512]):
+        super(unet_model_gelu,self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.conv1 = encoding_block_gelu(3,features[0])
+        self.conv2 = encoding_block_gelu(features[0],features[1])
+        self.conv3 = encoding_block_gelu(features[1],features[2])
+        self.conv4 = encoding_block_gelu(features[2],features[3])
+        self.conv5 = encoding_block_gelu(features[3]*2,features[3])
+        self.conv6 = encoding_block_gelu(features[3],features[2])
+        self.conv7 = encoding_block_gelu(features[2],features[1])
+        self.conv8 = encoding_block_gelu(features[1],features[0])        
+        self.tconv1 = nn.ConvTranspose2d(features[-1]*2, features[-1], kernel_size=2, stride=2)
+        self.tconv2 = nn.ConvTranspose2d(features[-1], features[-2], kernel_size=2, stride=2)
+        self.tconv3 = nn.ConvTranspose2d(features[-2], features[-3], kernel_size=2, stride=2)
+        self.tconv4 = nn.ConvTranspose2d(features[-3], features[-4], kernel_size=2, stride=2)        
+        self.bottleneck = encoding_block_gelu(features[3],features[3]*2)
+        self.final_layer = nn.Conv2d(features[0],out_channels,kernel_size=1)
+    def forward(self,x):
+        skip_connections = []
+        x = self.conv1(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        x = self.tconv1(x)
+        x = torch.cat((skip_connections[0], x), dim=1)
+        x = self.conv5(x)
+        x = self.tconv2(x)
+        x = torch.cat((skip_connections[1], x), dim=1)
+        x = self.conv6(x)
+        x = self.tconv3(x)
+        x = torch.cat((skip_connections[2], x), dim=1)
+        x = self.conv7(x)        
+        x = self.tconv4(x)
+        x = torch.cat((skip_connections[3], x), dim=1)
+        x = self.conv8(x)
+        x = self.final_layer(x)
+        return x
+
+      
+########################################################
+################ UNET Resnet Backbone ##################
+########################################################
+
+resnet = torchvision.models.resnet.resnet50(pretrained=True)
+
+class ConvBlock(nn.Module):
+    """
+    Helper module that consists of a Conv -> BN -> ReLU
+    """
+
+    def __init__(self, in_channels, out_channels, padding=1, kernel_size=3, stride=1, with_nonlinearity=True):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, padding=padding, kernel_size=kernel_size, stride=stride)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.with_nonlinearity = with_nonlinearity
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.with_nonlinearity:
+            x = self.relu(x)
+        return x
+
+
+class Bridge(nn.Module):
+    """
+    This is the middle layer of the UNet which just consists of some
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.bridge = nn.Sequential(
+            ConvBlock(in_channels, out_channels),
+            ConvBlock(out_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.bridge(x)
+
+
+class UpBlockForUNetWithResNet50(nn.Module):
+    """
+    Up block that encapsulates one up-sampling step which consists of Upsample -> ConvBlock -> ConvBlock
+    """
+
+    def __init__(self, in_channels, out_channels, up_conv_in_channels=None, up_conv_out_channels=None,
+                 upsampling_method="conv_transpose"):
+        super().__init__()
+
+        if up_conv_in_channels == None:
+            up_conv_in_channels = in_channels
+        if up_conv_out_channels == None:
+            up_conv_out_channels = out_channels
+
+        if upsampling_method == "conv_transpose":
+            self.upsample = nn.ConvTranspose2d(up_conv_in_channels, up_conv_out_channels, kernel_size=2, stride=2)
+        elif upsampling_method == "bilinear":
+            self.upsample = nn.Sequential(
+                nn.Upsample(mode='bilinear', scale_factor=2),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1)
+            )
+        self.conv_block_1 = ConvBlock(in_channels, out_channels)
+        self.conv_block_2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, up_x, down_x):
+        """
+
+        :param up_x: this is the output from the previous up block
+        :param down_x: this is the output from the down block
+        :return: upsampled feature map
+        """
+        x = self.upsample(up_x)
+        x = torch.cat([x, down_x], 1)
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        return x
+
+
+class UNetWithResnet50Encoder(nn.Module):
+    DEPTH = 6
+
+    def __init__(self, n_classes=9):
+        super().__init__()
+        resnet = torchvision.models.resnet.resnet50(pretrained=True)
+        down_blocks = []
+        up_blocks = []
+        self.input_block = nn.Sequential(*list(resnet.children()))[:3]
+        self.input_pool = list(resnet.children())[3]
+        for bottleneck in list(resnet.children()):
+            if isinstance(bottleneck, nn.Sequential):
+                down_blocks.append(bottleneck)
+        self.down_blocks = nn.ModuleList(down_blocks)
+        self.bridge = Bridge(2048, 2048)
+        up_blocks.append(UpBlockForUNetWithResNet50(2048, 1024))
+        up_blocks.append(UpBlockForUNetWithResNet50(1024, 512))
+        up_blocks.append(UpBlockForUNetWithResNet50(512, 256))
+        up_blocks.append(UpBlockForUNetWithResNet50(in_channels=128 + 64, out_channels=128,
+                                                    up_conv_in_channels=256, up_conv_out_channels=128))
+        up_blocks.append(UpBlockForUNetWithResNet50(in_channels=64 + 3, out_channels=64,
+                                                    up_conv_in_channels=128, up_conv_out_channels=64))
+
+        self.up_blocks = nn.ModuleList(up_blocks)
+
+        self.out = nn.Conv2d(64, n_classes, kernel_size=1, stride=1)
+
+    def forward(self, x, with_output_feature_map=False):
+        pre_pools = dict()
+        pre_pools[f"layer_0"] = x
+        x = self.input_block(x)
+        pre_pools[f"layer_1"] = x
+        x = self.input_pool(x)
+
+        for i, block in enumerate(self.down_blocks, 2):
+            x = block(x)
+            if i == (UNetWithResnet50Encoder.DEPTH - 1):
+                continue
+            pre_pools[f"layer_{i}"] = x
+
+        x = self.bridge(x)
+
+        for i, block in enumerate(self.up_blocks, 1):
+            key = f"layer_{UNetWithResnet50Encoder.DEPTH - 1 - i}"
+            x = block(x, pre_pools[key])
+        output_feature_map = x
+        x = self.out(x)
+        del pre_pools
+        if with_output_feature_map:
+            return x, output_feature_map
+        else:
+            return x
+
+model = UNetWithResnet50Encoder()
+
+
+############################################
+############### HSI UNET ###################
+############################################
+
+class preprocessing_block(nn.Module):
+    def __init__(self, channels_3):
+        super(preprocessing_block,self).__init__()
+        model = []
+        model.append(nn.Conv2d(45, 30, 1, 1, padding=0, bias=False))
+        model.append(nn.BatchNorm2d(30))
+        model.append(nn.GELU())
+        model.append(nn.Conv2d(30, 15, 1, 1, padding=0, bias=False))
+        model.append(nn.BatchNorm2d(15))
+        model.append(nn.GELU())
+        model.append(nn.Conv2d(15, 10, 1, 1, padding=0, bias=False))
+        model.append(nn.BatchNorm2d(10))
+        model.append(nn.GELU())
+        if channels_3==3:
+            model.append(nn.Conv2d(10, 3, 1, 1, padding=0, bias=False))
+            model.append(nn.BatchNorm2d(3))
+            model.append(nn.GELU())
+        self.preprocess = nn.Sequential(*model)
+    def forward(self, x):
+        return self.preprocess(x)
+
+  class hsi_unet_model_gelu(nn.Module):
+    def __init__(self, in_channels, out_channels=9, features=[64, 128, 256, 512]):
+        super(hsi_unet_model_gelu,self).__init__()
+        self.preprocess = preprocessing_block(in_channels)
+        self.pool = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.conv1 = encoding_block_gelu(in_channels,features[0])
+        self.conv2 = encoding_block_gelu(features[0],features[1])
+        self.conv3 = encoding_block_gelu(features[1],features[2])
+        self.conv4 = encoding_block_gelu(features[2],features[3])
+        self.conv5 = encoding_block_gelu(features[3]*2,features[3])
+        self.conv6 = encoding_block_gelu(features[3],features[2])
+        self.conv7 = encoding_block_gelu(features[2],features[1])
+        self.conv8 = encoding_block_gelu(features[1],features[0])
+        self.tconv1 = nn.ConvTranspose2d(features[-1]*2, features[-1], kernel_size=2, stride=2)
+        self.tconv2 = nn.ConvTranspose2d(features[-1], features[-2], kernel_size=2, stride=2)
+        self.tconv3 = nn.ConvTranspose2d(features[-2], features[-3], kernel_size=2, stride=2)
+        self.tconv4 = nn.ConvTranspose2d(features[-3], features[-4], kernel_size=2, stride=2)
+        self.bottleneck = encoding_block_gelu(features[3],features[3]*2)
+        self.final_layer = nn.Conv2d(features[0],out_channels,kernel_size=1)
+    def forward(self,x):
+        x = self.preprocess(x)
+        skip_connections = []
+        x = self.conv1(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        x = self.tconv1(x)
+        x = torch.cat((skip_connections[0], x), dim=1)
+        x = self.conv5(x)
+        x = self.tconv2(x)
+        x = torch.cat((skip_connections[1], x), dim=1)
+        x = self.conv6(x)
+        x = self.tconv3(x)
+        x = torch.cat((skip_connections[2], x), dim=1)
+        x = self.conv7(x)
+        x = self.tconv4(x)
+        x = torch.cat((skip_connections[3], x), dim=1)
+        x = self.conv8(x)
+        x = self.final_layer(x)
+        return x
