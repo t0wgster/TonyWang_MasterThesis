8e48b68e739c7eadaac9a7249ea22814e695b79e
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-05-22 14:18:32 +0200

    Created constants.py
    constants for all my ipynb notebooks

---
+++ constants.py
@@ -0,0 +1,7 @@
+#color values for complete classes
+num_unique_values_long = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+colors_long = ['black',          'white', 'green',  'red',  'cyan',          'blue',      'darkred',     'pink',     'navy', 'orange', ]
+classes_long = ['background', 'chicken_front', 'chicken_back', 'Blood', 'Bones', 'SurfaceDefect', 'Discoloring', 'Scalding', 'Deformed', 'Fat/Skin']
+txt_colors_long=['\033[0mblack', '\033[94mwhite', '\033[32mgreen','\033[91mred', 
+                 '\033[96mcyan', '\033[94mblue', '\\033[31mdarkred',
+                 '\033[95mpink' ,'\033[34mnavy' , '\033[38;2;255;165;0morange']
