99aa6c456ad7207cdecf41df4737c49722e5cc0b
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-06-04 15:29:45 +0200

    Update models.py

--- models.py
+++ models.py
@@ -303,6 +303,56 @@ class preprocessing_block(nn.Module):
     def forward(self, x):
         return self.preprocess(x)
 
+class hsi_unet_model_gelu_pca(nn.Module):
+    def __init__(self, in_channels, out_channels=9, features=[64, 128, 256, 512]):
+        super(hsi_unet_model_gelu,self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.conv1 = encoding_block_gelu(in_channels,features[0])
+        self.conv2 = encoding_block_gelu(features[0],features[1])
+        self.conv3 = encoding_block_gelu(features[1],features[2])
+        self.conv4 = encoding_block_gelu(features[2],features[3])
+        self.conv5 = encoding_block_gelu(features[3]*2,features[3])
+        self.conv6 = encoding_block_gelu(features[3],features[2])
+        self.conv7 = encoding_block_gelu(features[2],features[1])
+        self.conv8 = encoding_block_gelu(features[1],features[0])
+        self.tconv1 = nn.ConvTranspose2d(features[-1]*2, features[-1], kernel_size=2, stride=2)
+        self.tconv2 = nn.ConvTranspose2d(features[-1], features[-2], kernel_size=2, stride=2)
+        self.tconv3 = nn.ConvTranspose2d(features[-2], features[-3], kernel_size=2, stride=2)
+        self.tconv4 = nn.ConvTranspose2d(features[-3], features[-4], kernel_size=2, stride=2)
+        self.bottleneck = encoding_block_gelu(features[3],features[3]*2)
+        self.final_layer = nn.Conv2d(features[0],out_channels,kernel_size=1)
+    def forward(self,x):
+        x = self.preprocess(x)
+        skip_connections = []
+        x = self.conv1(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        x = self.tconv1(x)
+        x = torch.cat((skip_connections[0], x), dim=1)
+        x = self.conv5(x)
+        x = self.tconv2(x)
+        x = torch.cat((skip_connections[1], x), dim=1)
+        x = self.conv6(x)
+        x = self.tconv3(x)
+        x = torch.cat((skip_connections[2], x), dim=1)
+        x = self.conv7(x)
+        x = self.tconv4(x)
+        x = torch.cat((skip_connections[3], x), dim=1)
+        x = self.conv8(x)
+        x = self.final_layer(x)
+        return x
+
 class hsi_unet_model_gelu(nn.Module):
     def __init__(self, in_channels, out_channels=9, features=[64, 128, 256, 512]):
         super(hsi_unet_model_gelu,self).__init__()
