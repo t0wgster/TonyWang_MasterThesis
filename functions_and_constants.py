bb807f4d08e4fcb5270d6bd52abf9b3a3c287dd4
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-06-06 21:59:24 +0200

    Update functions_and_constants.py

--- functions_and_constants.py
+++ functions_and_constants.py
@@ -337,6 +337,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 NUM_UNIQUE_VALUES_LONG = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 COLORS_LONG = ['black',          'white', 'green',  'red',  'cyan',          'blue',      'darkred',     'pink',     'navy', 'orange', ]
 CLASSES_LONG = ['background', 'chicken_front', 'chicken_back', 'Blood', 'Bones', 'SurfaceDefect', 'Discoloring', 'Scalding', 'Deformed', 'Fat/Skin']
+CLASSES_LONG = ['background', 'chicken_front', 'chicken_back', 'Blood', 'Bones', 'SurfaceDefect', 'Discoloring', 'Scalding', 'Fat/Skin']
 TXT_COLORS_LONG=['\033[0mblack', '\033[94mwhite', '\033[32mgreen','\033[91mred', 
                  '\033[96mcyan', '\033[94mblue', '\\033[31mdarkred',
                  '\033[95mpink' ,'\033[34mnavy' , '\033[38;2;255;165;0morange']
