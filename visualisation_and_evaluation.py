16892df9ded2b3940ccaef1da5a84efb560d10f7
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-06-06 10:11:18 +0200

    Update visualisation_and_evaluation.py

--- visualisation_and_evaluation.py
+++ visualisation_and_evaluation.py
@@ -167,6 +167,59 @@ def rgb_visualize_prediction_vs_ground_truth_single_images_overlay_postprocessed
 
     plt.show()
 
+def visualize_prediction_vs_ground_truth_overlay_all_sources(rgb_img, hsi_img, truth_mask, pred_mask, data_source):
+
+    if data_source == 'rgb':
+
+        fig , axs =  plt.subplots(1, 3, figsize=(18, 12))
+
+        axs[0].set_title('Plain Image RGB')
+        axs[1].set_title('Image with Ground Truth')
+        axs[2].set_title('Image with Prediction')
+
+        axs[0].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+        axs[1].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+        axs[2].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+
+        axs[1].imshow(np.asarray(truth_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+        axs[2].imshow(np.asarray(pred_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+
+    elif data_source == 'hsi':
+
+        fig , axs =  plt.subplots(1, 4, figsize=(18, 12))
+
+        axs[0].set_title('Plain Image HSI')
+        axs[1].set_title('Image with Ground Truth')
+        axs[2].set_title('Image with Prediction')
+        axs[3].set_title('RGB Reference')
+
+        axs[0].imshow(np.asarray(hsi_img.to('cpu').permute(1,2,0))[:,:,0])
+        axs[1].imshow(np.asarray(hsi_img.to('cpu').permute(1,2,0))[:,:,0])
+        axs[2].imshow(np.asarray(hsi_img.to('cpu').permute(1,2,0))[:,:,0])
+        axs[3].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+
+        axs[1].imshow(np.asarray(truth_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+        axs[2].imshow(np.asarray(pred_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+
+    elif data_source == 'sf':
+
+        fig , axs =  plt.subplots(1, 4, figsize=(18, 12))
+
+        axs[0].set_title('Plain Image RGB')
+        axs[1].set_title('Plain Image HSI')
+        axs[2].set_title('Image with Ground Truth')
+        axs[3].set_title('Image with Prediction')
+
+        axs[0].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+        axs[1].imshow(np.asarray(hsi_img.to('cpu').permute(1,2,0))[:,:,0])
+        axs[2].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+        axs[3].imshow(np.asarray(rgb_img.to('cpu').permute(1,2,0)))
+
+        axs[2].imshow(np.asarray(truth_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+        axs[3].imshow(np.asarray(pred_mask.to('cpu')), cmap=cmap_long, norm=norm_long, alpha=0.3)
+
+    plt.show()
+
 def intersection_and_union_all_classes(truth_mask, pred_mask, N_CLASS=N_CLASSES, SINGLE_PREDICTION=False):
     
     intersection_list=[]
@@ -346,6 +399,153 @@ def capture_model_metrics_pixelwise_and_confusion_matrix(model, test_dataset_fin
             plt.show()
 
             return test_ds_union, test_ds_intersection, test_ds_numerator, test_ds_denominator, img_iou, img_dice
+
+def capture_model_metrics_pixelwise_and_confusion_matrix_sf(model, test_dataset_final, data_source, visualize = True, 
+                                                         confusion_matrix = True, norm_mode = 'pred', mask_shape=(320,320),
+                                                         smooth=1e-8):
+
+    test_ds_union = [0,0,0,0,0,0,0,0,0,0]
+    test_ds_intersection = [0,0,0,0,0,0,0,0,0,0]
+    test_ds_numerator = [0,0,0,0,0,0,0,0,0,0]
+    test_ds_denominator = [0,0,0,0,0,0,0,0,0,0]
+    img_dice = []
+    img_iou = []
+
+    ground_truth_all_images=np.zeros((mask_shape[0], mask_shape[1], len(test_dataset_final)))
+    prediction_all_images=np.zeros((mask_shape[0], mask_shape[1], len(test_dataset_final)))
+    
+    with torch.no_grad():
+        model.eval()
+
+        for n, batch in enumerate(test_dataset_final):
+
+            # model prediction
+            if data_source=='rgb' or data_source=='hsi':
+                img,  mask = batch
+                img = img.to(DEVICE).unsqueeze(0)
+                mask = mask.to(DEVICE)
+
+                softmax = nn.Softmax(dim=1)
+
+                preds = torch.argmax(softmax(model(img.float())),axis=1).to('cpu').squeeze(0)
+
+            elif data_source=='sf':
+                rgb_img, hsi_img, mask = batch
+
+                rgb_img = rgb_img.to(DEVICE).unsqueeze(0)
+                hsi_img = hsi_img.to(DEVICE).unsqueeze(0)
+
+                mask = mask.to(DEVICE)
+
+                softmax = nn.Softmax(dim=1)
+
+                preds = torch.argmax(softmax(model(rgb_img.float(), hsi_img)),axis=1).to('cpu').squeeze(0)
+
+                #print(rgb_img.shape)
+                #print(hsi_img.shape)
+                #print(mask.shape)
+                #print(preds.shape)
+
+            # convert torch tensor to numpy array
+            prediction_all_images[:,:,n] = preds.numpy()
+            ground_truth_all_images[:,:,n] = mask.to('cpu').numpy()
+
+            #calculate dice and iou score
+            is_list, u_list=intersection_and_union_all_classes(mask, preds, SINGLE_PREDICTION=True)
+            n_list, d_list=dice_values_all_classes(mask, preds, SINGLE_PREDICTION=True)
+
+
+
+            if data_source=='rgb' or data_source=='hsi':
+                visualize_prediction_vs_ground_truth_overlay_all_sources(img.squeeze(0), None, mask, preds.squeeze(0), data_source)
+            elif data_source=='sf':
+                visualize_prediction_vs_ground_truth_overlay_all_sources(rgb_img.squeeze(0), hsi_img.squeeze(0), mask, preds.squeeze(0), data_source)
+
+            print('IOU')
+            for i in range(len(NUM_UNIQUE_VALUES_LONG)):
+                if is_ground_truth_empty(mask)[i] and is_prediction_empty(preds)[i]:
+                    print(f'{TXT_COLORS_LONG_COLOR_ONLY[i]} - {CLASSES_LONG[i]}: Empty')
+                else:
+                    print(f'{TXT_COLORS_LONG_COLOR_ONLY[i]} - {CLASSES_LONG[i]}: {is_list[i]/(u_list[i]+smooth):.4f}')
+
+                    #tracking class average of iou across all images
+                    test_ds_union[i] += u_list[i]
+                    test_ds_intersection[i] += is_list[i]
+                    
+            print(TXT_COLORS_LONG_COLOR_ONLY[0]+ 'x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x')
+
+            print('Dice')
+            for i in range(len(NUM_UNIQUE_VALUES_LONG)):
+                if is_ground_truth_empty(mask)[i] and is_prediction_empty(preds)[i]:
+                    print(f'{TXT_COLORS_LONG_COLOR_ONLY[i]} - {CLASSES_LONG[i]}: Empty')
+                else:
+                    print(f'{TXT_COLORS_LONG_COLOR_ONLY[i]} - {CLASSES_LONG[i]}: {n_list[i]/(d_list[i]+smooth):.4f}')
+
+                    #tracking class average of iou across all images
+                    test_ds_numerator[i] += n_list[i]
+                    test_ds_denominator[i] += d_list[i]
+    
+            print(TXT_COLORS_LONG_COLOR_ONLY[0])
+    
+            #only defects
+            # calculate iou and dice
+
+            intersection_array = np.array(is_list)
+            union_array = np.array(u_list)
+            numinator_array = np.array(n_list)
+            denominator_array = np.array(d_list)
+
+            iou_image_pixelwise = intersection_array[3:].sum()/(union_array[3:].sum()+smooth)
+            dice_image_pixelwise = numinator_array[3:].sum()/(denominator_array[3:].sum()+smooth)
+
+            print('Defects Only')
+            print(TXT_COLORS_LONG_COLOR_ONLY[0]+f'Image Average IoU: {iou_image_pixelwise:.4f}')
+            print(TXT_COLORS_LONG_COLOR_ONLY[0]+f'Image Average Dice: {dice_image_pixelwise:.4f}')
+
+            img_dice.append(dice_image_pixelwise)
+            img_iou.append(iou_image_pixelwise)
+
+            # frees up memeory every 10th image
+            if n%10:
+                torch.cuda.empty_cache()
+
+        if confusion_matrix:
+            gt_flat = ground_truth_all_images.flatten()
+            prediction_flat = prediction_all_images.flatten()
+
+            fig, ax = plt.subplots(figsize=(10, 8))
+
+            conf=ConfusionMatrixDisplay.from_predictions(gt_flat, prediction_flat, display_labels=CLASSES_LONG, normalize=norm_mode, 
+                                                         ax=ax, xticks_rotation='vertical')
+
+            plt.show()
+
+            return test_ds_union, test_ds_intersection, test_ds_numerator, test_ds_denominator, img_iou, img_dice
+        
+def calculate_model_metrics(test_ds_intersection, 
+                            test_ds_union, 
+                            test_ds_numerator, 
+                            test_ds_denominator,
+                            defects_only):
+
+    if defects_only:
+        print('Average IoU over entire Test Dataset: '+f'{np.sum(np.array(test_ds_intersection[3:]))/(np.sum(np.array(test_ds_union)[3:])+1e-06):.4f}')
+        print('Average Dice Score over entire Test Dataset: '+f'{np.sum(np.array(test_ds_numerator[3:]))/(np.sum(np.array(test_ds_denominator[3:]))+1e-06):.4f}')
+
+    else:
+        print('Average IoU over entire Test Dataset: '+f'{np.sum(np.array(test_ds_intersection))/(np.sum(np.array(test_ds_union))+1e-06):.4f}')
+        print('Average Dice Score over entire Test Dataset: '+f'{np.sum(np.array(test_ds_numerator))/(np.sum(np.array(test_ds_denominator))+1e-06):.4f}')
+
+    print('--Class Average IoU--')
+    for i in range(len(CLASSES_LONG)):
+
+        print(f'{CLASSES_LONG[i]}: {test_ds_intersection[i]/(test_ds_union[i]+1e-06):.4f}')
+
+    print('--Class Average Dice Score--')
+
+    for i in range(len(CLASSES_LONG)):
+
+        print(f'{CLASSES_LONG[i]}: {test_ds_numerator[i]/(test_ds_denominator[i]+1e-06):.4f}')
         
 def calculate_model_metrics(test_ds_intersection, 
                             test_ds_union, 
