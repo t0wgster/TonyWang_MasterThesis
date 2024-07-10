b4b1b0f67ca3ded939da9740de022a0af0e90e66
t0wgster <125188064+t0wgster@users.noreply.github.com>
2024-07-10 13:20:03 +0200

    Update visualisation_and_evaluation.py

--- visualisation_and_evaluation.py
+++ visualisation_and_evaluation.py
@@ -28,7 +28,7 @@ from torchsummary import summary
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, f1_score
 import skimage.io as skio # lighter dependency than tensorflow for working with our tensors/arrays
 
-from thesis_constants.functions_and_constants import *
+from TonyWang_MasterThesis.functions_and_constants import *
 
 ##################################################
 ############### Visualisation ####################
